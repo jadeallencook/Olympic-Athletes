ba550e1717987b2dca79d141978f6096aeeb4c64
Jade Allen Cook <jcook@deseretnews.com>
2018-01-25 12:58:02 -0700

    📐 UI/UX tweaks and mobile updates

--- assets/app.js
+++ assets/app.js
@@ -1,26 +1,57 @@
 (function () {
   var current = 0;
   var athlete = data.athletes[current];
+  var eventNum = 5;
+  var medalsNum = 5;
   function updateText(id, text) {
     var elem = document.getElementById(id);
-    elem.innerText = text;
+    elem.innerHTML = text;
   }
   function loadEvents() {
     var container = document.getElementById('events-container');
     container.innerHTML = '';
-    for (var x = 1; x <= 5; x++) {
+    for (var x = 1; x <= eventNum; x++) {
       var event = athlete['event-' + x];
       if (event) {
         if (data.events[event]) {
           event = data.events[event];
-          container.innerHTML += event;
+          var date = event.match(/\((.*?)\)/);
+          date = date[0].replace('(', '').replace(')', '');
+          date = new Date(date);
+          var month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'][date.getMonth()];
+          event = event.replace(/\((.*?)\)/, '');
+          container.innerHTML += '<div class="olympic-date" data-desc="' + event + '" id="event-' + x + '"><span class="month">' + month + '</span><span class="date">' + date.getDate() + '</span></div>';
         }
+      }
+    }
+    for (var x = 1; x <= 5; x++) {
+      var container = document.getElementById('event-' + x);
+      if (container) {
+        container.onclick = function() {
+          var event = this.getAttribute('data-desc');
+          event = '<b>' + event.replace(':', ' - </b> ')
+          document.getElementById('event-info').innerHTML = event;
+        }
+      }
+    }
+  }
+  function loadMedals() {
+    var container = document.getElementById('medals-container');
+    container.innerHTML = '';
+    for (var x = 1; x <= 5; x++) {
+      var medal = athlete['medal-' + x];
+      if (medal) {
+        medal = '<b>' + medal.replace(':', ' - </b>');
+        container.innerHTML += medal + '<br />';
       }
     }
   }
   function loadProfile() {
     updateText('profile-name', athlete.name);
     updateText('profile-about', athlete.about);
+    updateText('profile-hometown', athlete.hometown + ', UT');
+    document.getElementById('event-info').innerHTML = 'Click an event to see info!';
+    document.getElementById('profile-read-more').setAttribute('href', athlete.link);
     document.getElementById('profile-picture').style.backgroundImage = 'url(' + athlete.photo + ')';
     for (var x = 0; x < 5; x++) {
       var num = ((x + 1) + current) % data.athletes.length;
@@ -29,6 +60,7 @@
       preview.setAttribute('data-val', num);
     }
     loadEvents();
+    loadMedals();
   }
   function router(selected) {
     var sections = ['about', 'medals', 'events'];
